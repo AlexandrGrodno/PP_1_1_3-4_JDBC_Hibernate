84a59aa7fde097d1b9cd2b391dcb2771715bf966
AlexandrGrodno <avgust_sa@mail.ru>
2023-07-09 23:13:21 +0300

    test BD

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/newschema";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "52532137";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME,DB_PASSWORD);
+            System.out.println("OK");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
 }
+
