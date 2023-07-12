99552419203890441433313f7176210e38898dd3
AlexandrGrodno <avgust_sa@mail.ru>
2023-07-12 16:24:38 +0300

    process

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
@@ -15,24 +16,26 @@ public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
 
-        UserService userService = new UserServiceImpl();
-        userService.createUsersTable();
-        User user1 = new User("Ivan","Ivanov", (byte) 10);
-        User user2 = new User("Petr","Petrov", (byte) 11);
-        User user3 = new User("Lesha","Leshin", (byte) 12);
-        User user4 = new User("Masha","Mashkova", (byte) 13);
-        userService.saveUser(user1.getName(),user1.getLastName(),user1.getAge());
-        System.out.println("User с именем "+user1.getName()+" добавлен в базу данных");
-        userService.saveUser(user2.getName(),user2.getLastName(),user2.getAge());
-        System.out.println("User с именем "+user2.getName()+" добавлен в базу данных");
-        userService.saveUser(user3.getName(),user3.getLastName(),user3.getAge());
-        System.out.println("User с именем "+user3.getName()+" добавлен в базу данных");
-        userService.saveUser(user4.getName(),user4.getLastName(),user4.getAge());
-        System.out.println("User с именем "+user4.getName()+" добавлен в базу данных");
+//        UserService userService = new UserServiceImpl();
+//        userService.createUsersTable();
+//        User user1 = new User("Ivan","Ivanov", (byte) 10);
+//        User user2 = new User("Petr","Petrov", (byte) 11);
+//        User user3 = new User("Lesha","Leshin", (byte) 12);
+//        User user4 = new User("Masha","Mashkova", (byte) 13);
+//        userService.saveUser(user1.getName(),user1.getLastName(),user1.getAge());
+//        System.out.println("User с именем "+user1.getName()+" добавлен в базу данных");
+//        userService.saveUser(user2.getName(),user2.getLastName(),user2.getAge());
+//        System.out.println("User с именем "+user2.getName()+" добавлен в базу данных");
+//        userService.saveUser(user3.getName(),user3.getLastName(),user3.getAge());
+//        System.out.println("User с именем "+user3.getName()+" добавлен в базу данных");
+//        userService.saveUser(user4.getName(),user4.getLastName(),user4.getAge());
+//        System.out.println("User с именем "+user4.getName()+" добавлен в базу данных");
 
-        System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        //System.out.println(userService.getAllUsers());
+        UserDaoHibernateImpl us = new UserDaoHibernateImpl();
+        us.createUsersTable();
+       // userService.cleanUsersTable();
+        //userService.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,10 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -12,6 +16,13 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Session session = Util.getConnectionHibernate().openSession();
+        User user1 = new User("2qw","2qwqw",(byte)22);
+        Transaction transaction = session.beginTransaction();
+        session.createQuery("CREATE TABLE IF NOT EXISTS users (id BIGINT NOT NULL AUTO_INCREMENT, firstName" +
+                " VARCHAR(255), lastName VARCHAR(255), age INT(3), PRIMARY KEY (id))").executeUpdate();
+        transaction.commit();
+
 
     }
 
